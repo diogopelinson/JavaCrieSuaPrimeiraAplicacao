d1fa677ffae811c2c1ff68bacd277f57e16620d8
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-07-18 21:21:54 -0300

    Inicial commit

---
+++ jardio/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este é o TelaViva");
+        System.out.println("Filme: Missão: Impossível – Acerto De Contas: Parte 2");
+        int releaseYear = 2023;
+
+        System.out.println("Ano de : " + releaseYear);
+        boolean includedInThePlan = true;
+        double filmNote = 8.5;
+
+        double average = (9.5 + 6.3 + 10.0 + 2.2) / 4;
+        System.out.println(average);
+
+        String synopsis;
+        synopsis = "Ethan Hunt e sua equipe devem rastrear uma nova arma que pode ameaçar toda a humanidade se cair em mãos erradas.";
+
+        System.out.println(synopsis);
+
+        int classification;
+        classification = (int) (average / 2);
+
+        System.out.println(classification);
+
+
+
+
+
+        double precoOriginal = 150.0;
+        double percentualDesconto = 10.0;
+
+        double desconto = precoOriginal / percentualDesconto;
+
+        System.out.println(precoOriginal - desconto);
+
+
+
+    }
+}
